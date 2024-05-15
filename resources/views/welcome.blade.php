e0285b04e4fbadc6f9402121f9015db8e7007500
Karen H <karen.hernandez.restrepo@correounivalle.edu.co>
2024-05-15 09:39:26 -0500

    Vista de productos estatica y login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,20 +41,20 @@
                                         Log in
                                     </a>
 
-                                    @if (Route::has('register'))
+                                    {{-- @if (Route::has('register'))
                                         <a
                                             href="{{ route('register') }}"
                                             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                         >
                                             Register
                                         </a>
-                                    @endif
+                                    @endif --}}
                                 @endauth
                             </nav>
                         @endif
                     </header>
 
-                    <main class="mt-6">
+                    {{-- <main class="mt-6">
                         <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
                             <a
                                 href="https://laravel.com/docs"
@@ -160,7 +160,7 @@
                                 </div>
                             </div>
                         </div>
-                    </main>
+                    </main> --}}
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
